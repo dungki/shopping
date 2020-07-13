41d55b73567d4d7a1c8c8c0c4771c53d5c65aed2
Trần Mạnh Dũng <dungtmth1907043@fpt.edu.vn>
2020-07-13 20:07:45 +0700

    first update

---
+++ database/seeds/POSSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class POSSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $currentTime = date('Y-m-d H:i:s');
+
+		//Fake khach hang
+		for ($i = 0; $i < 5; $i++) {
+			DB::table('customers')->insert([
+					'fullname'     => 'TRAN VAN '.$i,
+					'email'        => $i.'@gokisoft.com',
+					'phone_number' => $i,
+					'address'      => 'DC '.$i,
+					'birthday'     => '1999-02-02',
+					'created_at'   => $currentTime,
+					'updated_at'   => $currentTime
+				]);
+		}
+
+		//Fake san pham
+		for ($i = 0; $i < 3; $i++) {
+			$category = \App\Category::create([
+					'category_name' => 'Danh Muc '.$i
+				]);
+
+			for ($j = 1; $j < 6; $j++) {
+				DB::table('products')->insert([
+						'title'       => 'San Pham '.$j,
+						'price'       => $j*10000,
+						'thumbnail'   => 'https://bom.to/mHoJtY',
+						'content'     => 'Noi dung '.$j,
+						'id_category' => $category->id,
+						'created_at'  => $currentTime,
+						'updated_at'  => $currentTime
+					]);
+			}
+        }
+    }
+}
